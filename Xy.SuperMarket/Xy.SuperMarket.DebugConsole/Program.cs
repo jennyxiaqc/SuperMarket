b519e0fe6c621e08d24ee8a0d5aa60784297ce35
jennyxiaqc <xiayun78@hotmail.com>
2020-03-12 19:43:04 -0400

    1[CHG]debugConsole.program.cs

--- Xy.SuperMarket/Xy.SuperMarket.DebugConsole/Program.cs
+++ Xy.SuperMarket/Xy.SuperMarket.DebugConsole/Program.cs
@@ -13,15 +13,36 @@ namespace Xy.SuperMarket.DebugConsole
         static void Main(string[] args)
         {
             using (var ctx = new EFDbContext())
+            //{
+            //    var product = new Product { Name = "Potato", Catergory = "Vegitable",Description="product of Quebec", Price = 6.99m, Unit = "10Lb/bag" };
+            //    ctx.Products.Add(product);
+            //    product = new Product { Name = "Tomato", Catergory = "vegitable", Description = "product of USA",Price = 2.99m, Unit = "Lb" };
+            //    ctx.Products.Add(product);
+            //    product = new Product { Name = "NestCape Cofee", Catergory = "Grocery", Description = "product of Mexico", Price = 8.99m, Unit = "500g" };
+            //    ctx.Products.Add(product);
+            //    ctx.SaveChanges();
+            //}
             {
-                var product = new Product { Name = "Potato", Catergory = "Vegitable",Description="product of Quebec", Price = 6.99m, Unit = "10Lb/bag" };
-                ctx.Products.Add(product);
-                product = new Product { Name = "Tomato", Catergory = "vegitable", Description = "product of USA",Price = 2.99m, Unit = "Lb" };
-                ctx.Products.Add(product);
-                product = new Product { Name = "NestCape Cofee", Catergory = "Grocery", Description = "product of Mexico", Price = 8.99m, Unit = "500g" };
-                ctx.Products.Add(product);
+                var productToRemove =
+                    ctx
+                    .Products
+                    .ToList()
+                    .Take(3)
+                    .ToList();
+
+                foreach (var item in productToRemove)
+                {
+                    ctx.Products.Remove(item);
+                }
                 ctx.SaveChanges();
+
+                foreach (var product in ctx.Products)
+                {
+                    Console.WriteLine(product.Name);
+                }
             }
+
+
             Console.WriteLine("done");
             Console.ReadLine();
             
